4366c022ca771f6518d808849baddcd188884410
Fabio Vergani <fabio.vergani.1975@gmail.com>
2015-11-12 19:16:32 +0100

    Create GlobalPattern.js

---
+++ GlobalPattern.js
@@ -0,0 +1,14 @@
+(function(){'use strict';
+ var e=arguments[0];
+ if(e===undefined){
+	return;
+ }else{
+	var d=e.ownerDocument,isGlobal=(d===undefined?(d=e.document,1):0),
+	dE=d.documentElement,
+	dV=d.defaultView,
+	w=dV.window;
+	e=null;
+	//console.log(isGlobal,w,d,dE);
+ };
+
+})(window);
